611098bf91c1d5d174e7e3296342fe1ac1c9a6b0
Sjpeti97 <sjpeti97@gmail.com>
2021-10-19 14:34:39 +0200

    feat: refactor endpoint to handle multiple filters

--- src/main/java/com/codecool/shop/controller/FilterController.java
+++ src/main/java/com/codecool/shop/controller/FilterController.java
@@ -2,8 +2,10 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.service.ProductService;
@@ -16,10 +18,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
-@WebServlet(urlPatterns ={"/products-by-category"})
-public class CategoryController extends HttpServlet {
+@WebServlet(urlPatterns ={"/filter-products"})
+public class FilterController extends HttpServlet {
 
     private final Gson gson = new Gson();
 
@@ -27,15 +30,29 @@ public class CategoryController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
+        SupplierDao supplierDao = SupplierDaoMem.getInstance();
+        ProductService productService = new ProductService(productDataStore,productCategoryDataStore, supplierDao);
         PrintWriter out = response.getWriter();
 
-        List<Product> productsByCategory = productService.getProductsForCategory(Integer.parseInt(request.getParameter("category_id")));
+        var productsByCategory = getProductsByFilter(request.getParameter("filter"), Integer.parseInt(request.getParameter("id")), productService);
         String productsJsonString = this.gson.toJson(productsByCategory);
-        System.out.println(productsJsonString);
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         out.print(productsJsonString);
         out.flush();
     }
+
+    private List<Product> getProductsByFilter(String filer, int id, ProductService productService) {
+        List<Product> products = new ArrayList<>();
+        switch (filer) {
+            case "category":
+                products =  productService.getProductsForCategory(id);
+                break;
+            case "supplier":
+                products = productService.getProductsForSupplier(id);
+                break;
+        }
+        return products;
+    }
+
 }
