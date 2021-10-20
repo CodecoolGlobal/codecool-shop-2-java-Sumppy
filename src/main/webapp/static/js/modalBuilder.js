38e84a6de0f1f0a214e7d73e8f622d9e3cdfec3c
Totoo95 <juhtam95@gmail.com>
2021-10-20 14:05:08 +0200

    feat: modal design

--- src/main/webapp/static/js/modalBuilder.js
+++ src/main/webapp/static/js/modalBuilder.js
@@ -5,10 +5,22 @@ export function buildModal(id){
             <span class="close">&times</span>
         </div>
         <div class="modal-body">
-            <div class="product">valami</div>
-            <div class="product-quantity">
-                <a href="#" class="decrease-quantity">-</a><p class="quantity-amount">1</p><a href="#" class="increase-quantity">+</a>
+            <div class="quantity-container">
+                <div class="amount">    
+                    <p>Amount: </p>
+                </div>        
+                <div class="product-quantity">
+                    <a href="#" class="decrease-quantity">-</a>
+                    <p class="quantity-amount">1</p>
+                    <a href="#" class="increase-quantity">+</a>
+                </div>
             </div>
+            <div class="price-container">
+                <p class="price-amount">100</p>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <h3>Modal Footer</h3>
         </div>
     </div>`
 }
