15803365c08942a419e9924745c2e53ef3664a86
Sjpeti97 <sjpeti97@gmail.com>
2021-10-20 09:39:39 +0200

    feat: add new cart class

---
+++ src/main/java/com/codecool/shop/model/Cart.java
@@ -0,0 +1,40 @@
+package com.codecool.shop.model;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Cart {
+
+    private Map<Product, Integer> cart = new HashMap<>();
+    private static Cart instance = null;
+    private BigDecimal valueOfCart = null;
+
+    private Cart() {}
+
+    public static Cart getInstance() {
+        if (instance == null) {
+            instance = new Cart();
+        }
+        return instance;
+    }
+
+    public void updateProduct(Product product, int newQuantity) {
+        cart.put(product, newQuantity);
+        calculateCartValue();
+    }
+
+    public Map<Product, Integer> getCart() {
+        return cart;
+    }
+
+    private void calculateCartValue() {
+        for (Product product : cart.keySet()) {
+            valueOfCart = valueOfCart.add(product.getDefaultPrice().multiply(new BigDecimal(cart.get(product))));
+        }
+    }
+
+    public BigDecimal getValueOfCart() {
+        return valueOfCart;
+    }
+}
