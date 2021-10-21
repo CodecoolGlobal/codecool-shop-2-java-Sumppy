ff02248d71a02c969af8070ff8b2a1b9e82f645e
Totoo95 <juhtam95@gmail.com>
2021-10-21 21:22:13 +0200

    fix: fixed bug for sorting

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -35,13 +35,17 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(vehicle);
         ProductCategory weapon = new ProductCategory("Weapon", "War", "Used to kill enemy.");
         productCategoryDataStore.add(weapon);
+        ProductCategory humanoid = new ProductCategory("Human", "Slave", "A person, who serves all your needs");
+        productCategoryDataStore.add(humanoid);
+        ProductCategory everyday = new ProductCategory("Everyday", "Everyday", "Objects for everyday use");
+        productCategoryDataStore.add(everyday);
 
         //setting up products and printing it
         productDataStore.add(new Product("Tie-fighter", new BigDecimal("599"), "Cr", "Fantastic price. Barely used. Great for transportation. It is unarmed by the technical engineers of the Empire", vehicle, empire));
-        productDataStore.add(new Product("Slave from Tatooine", new BigDecimal("99"), "Cr", "Loyal. Great way to cut down your grass. Cheap price for cheap work.                                   ", vehicle, ryloth));
-        productDataStore.add(new Product("Battle Droid", new BigDecimal("30"), "Cr", "NOT WORKING. Comes without a weapon, great for melting down and forging something out of it.                  ", vehicle, empire));
-        productDataStore.add(new Product("Vader school kit", new BigDecimal("15"), "Cr", "Best merch in the Galaxy! Show Lord Vader some support by buying this kit!!                               ", vehicle, empire));
+        productDataStore.add(new Product("Slave from Tatooine", new BigDecimal("99"), "Cr", "Loyal. Great way to cut down your grass. Cheap price for cheap work.                                   ", humanoid, ryloth));
+        productDataStore.add(new Product("Battle Droid", new BigDecimal("30"), "Cr", "NOT WORKING. Comes without a weapon, great for melting down and forging something out of it.                  ", humanoid, empire));
+        productDataStore.add(new Product("Vader school kit", new BigDecimal("15"), "Cr", "Best merch in the Galaxy! Show Lord Vader some support by buying this kit!!                               ", everyday, empire));
         productDataStore.add(new Product("Clone trooper blaster", new BigDecimal("49"), "Cr", "Old but gold. It is not working but a great way to remember what killed the Jedi ;)                  ", weapon, empire));
-        productDataStore.add(new Product("Mercedes SW edition", new BigDecimal("499"), "Cr", "The best gag that the writer could find at 18:50 yesterday so don't even ask what the hek is this...", weapon, empire));
+        productDataStore.add(new Product("Mercedes SW edition", new BigDecimal("499"), "Cr", "The best gag that the writer could find at 18:50 yesterday so don't even ask what the hek is this...", vehicle, empire));
     }
 }
