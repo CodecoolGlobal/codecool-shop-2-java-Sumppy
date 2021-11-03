05b8eb146c1b3b6b6668332c365cf3452686f761
Sjpeti97 <sjpeti97@gmail.com>
2021-11-03 10:47:08 +0100

    Refactor: delete comments

--- src/main/java/com/codecool/shop/model/Email.java
+++ src/main/java/com/codecool/shop/model/Email.java
@@ -7,7 +7,7 @@ import java.util.Properties;
 public class Email {
 
     public static void send(String from,String password,String to,String sub,String msg){
-        //Get properties object
+
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.port", "465");
@@ -22,13 +22,13 @@ public class Email {
                         return new PasswordAuthentication(from,password);
                     }
                 });
-        //compose message
+
         try {
             MimeMessage message = new MimeMessage(session);
             message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));
             message.setSubject(sub);
             message.setText(msg);
-            //send message
+
             Transport.send(message);
             System.out.println("message sent successfully");
         } catch (MessagingException e) {throw new RuntimeException(e);}
