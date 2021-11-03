eb2187c4092f8d51f2d6c5cdb30a08abb7882bbb
Papp Bence <papp.bence15@gmail.com>
2021-11-03 13:54:42 +0100

    feat: db connection

---
+++ src/main/java/com/codecool/shop/dao/implementation/db/DatabaseManager.java
@@ -0,0 +1,38 @@
+package com.codecool.shop.dao.implementation.db;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private OrderDaoDb orderDaoDb;
+    private ProductCategoryDaoDb productCategoryDaoDb;
+    private ProductDaoDb productDaoDb;
+    private SupplierDaoDb supplierDaoDb;
+
+    public void setup() throws SQLException {
+        DataSource dataSource = connect();
+        orderDaoDb = new OrderDaoDb();
+        productCategoryDaoDb = new ProductCategoryDaoDb();
+        productDaoDb = new ProductDaoDb();
+        supplierDaoDb = new SupplierDaoDb();
+    }
+
+    private DataSource connect() throws SQLException{
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String dbName = System.getenv("DATA_BASE");
+        String user = System.getenv("DB_USER");
+        String password = System.getenv("DB_PASSWORD");
+
+        dataSource.setDatabaseName(dbName);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
+        return dataSource;
+    }
+}
