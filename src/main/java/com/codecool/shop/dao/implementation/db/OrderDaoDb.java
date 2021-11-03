347732dfddf3647d2fb556251fbaa34ad9305b78
Papp Bence <papp.bence15@gmail.com>
2021-11-03 13:35:09 +0100

    feat: create new dao classes for database operations

---
+++ src/main/java/com/codecool/shop/dao/implementation/db/OrderDaoDb.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.dao.implementation.db;
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Order;
+
+public class OrderDaoDb implements OrderDao {
+    @Override
+    public void add(Order order) {
+
+    }
+
+    @Override
+    public Order find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+}
